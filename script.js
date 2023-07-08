eecc04d0a08954ba6fcb0d0c4bbe70e649fc4ff3
Livni <llmc0383@gmail.com>
2023-07-07 22:11:42 -0600

    Tipos de datos en js

---
+++ script.js
@@ -0,0 +1,64 @@
+//Sintaxis básica
+
+    //Tipos de datos
+    //1. undefined (no esta definido)
+        let indefinido
+        console.log(indefinido);
+
+    //2. Booleano
+        let boolv = true;
+        console.log(boolv);
+
+        let boolf = false;
+        console.log(boolf);
+      
+    //3. Numerico
+        number = 83;
+        console.log(number);
+
+        number2 = 3.33;
+        console.log(number2);
+
+    //4. Cadena de texto
+        let cadena = "Hola, esto es una cadena de texto.";
+        console.log(cadena);
+        let cadena2 = "Hola, esto es mi tarea.";
+        console.log(cadena2);
+    
+    //5. Nulos
+        let datoNulo = null;
+        console.log(datoNulo);
+    
+    //6. Objetos
+        let persona = {
+            nombre: "Livni",
+            edad: 29,
+            mascota: "gato"
+        };
+      
+        console.log(persona.nombre); 
+        console.log(persona.edad); 
+        console.log(persona.mascota);
+
+        let barco = {
+            marca: "Kukulkan",
+            modelo: "Gc-1051",
+            capacidad: 2500,
+          };
+          
+        console.log(barco.marca);
+        console.log(barco.modelo); 
+        console.log(barco.capacidad); 
+        
+    //7. Arreglos
+        let frutas = ["manzana", "naranja", "plátano", "uva"];
+        
+        console.log(frutas[0]); // Se visualiza: manzana
+        console.log(frutas[2]); // Se visualiza: plátano
+        console.log(frutas.length); // Se visualiza: 4
+
+        let animales = ["perro", "gato", "elefante", "tigre", "león"];
+
+        console.log(animales[1]); // Se visualiza: gato
+        console.log(animales[3]); // Se visualiza: tigre
+        console.log(animales.length); // Se visualiza: 5
