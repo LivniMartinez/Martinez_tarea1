8abc5733103456a7bebd842aab97da2038c1b434
Livni <llmc0383@gmail.com>
2023-07-07 23:03:55 -0600

    se agrega concatenacion

--- script.js
+++ script.js
@@ -2,69 +2,75 @@
 
     //Tipos de datos
     //1. undefined (no esta definido)
-            //let indefinido
-            //console.log(indefinido);
+            let indefinido
+            console.log(indefinido);
 
     //2. Booleano
-            // let boolv = true;
-            // console.log(boolv);
+        let boolv = true;
+        console.log(boolv);
 
-            // let boolf = false;
-            // console.log(boolf);
+        let boolf = false;
+        console.log(boolf);
       
     //3. Numerico
-        // let number = 83;
-        // console.log(number);
+        let number = 83;
+        console.log(number);
 
-        // let number2 = 3.33;
-        // console.log(number2);
+        let number2 = 3.33;
+        console.log(number2);
 
     //4. Cadena de texto
-        // let cadena = "Hola, esto es una cadena de texto.";
-        // console.log(cadena);
-        // let cadena2 = "Hola, esto es mi tarea.";
-        // console.log(cadena2);
+        let cadena = "Hola, esto es una cadena de texto.";
+        console.log(cadena);
+        let cadena2 = "Hola, esto es mi tarea.";
+        console.log(cadena2);
     
     //5. Nulos
-        // let datoNulo = null;
-        // console.log(datoNulo);
+        let datoNulo = null;
+        console.log(datoNulo);
     
     //6. Objetos
-        // let persona = {
-        //     nombre: "Livni",
-        //     edad: 29,
-        //     mascota: "gato",
-        //     saludar: function (){
-        //         alert("hola soy Livni")
-        //     }
-        // };
+        let persona = {
+            nombre: "Livni",
+            edad: 29,
+            mascota: "gato",
+            saludar: function (){
+                alert("hola soy Livni")
+            }
+        };
       
-        // console.log(persona.nombre); 
-        // console.log(persona.edad); 
-        // console.log(persona.mascota);
+        console.log(persona.nombre); 
+        console.log(persona.edad); 
+        console.log(persona.mascota);
 
-        // let barco = {
-        //     marca: "Kukulkan",
-        //     modelo: "Gc-1051",
-        //     capacidad: 2500,
-        //     saludar: function (){
-        //         alert("hola soy un barco")
-        //     }
-        //   };
+        let barco = {
+            marca: "Kukulkan",
+            modelo: "Gc-1051",
+            capacidad: 2500,
+            saludar: function (){
+                alert("hola soy un barco")
+            }
+          };
           
-        // console.log(barco.marca);
-        // console.log(barco.modelo); 
-        // console.log(barco.capacidad); 
+        console.log(barco.marca);
+        console.log(barco.modelo); 
+        console.log(barco.capacidad); 
         
     //7. Arreglos
-        // let frutas = ["manzana", "naranja", "plátano", "uva"];
+        let frutas = ["manzana", "naranja", "plátano", "uva"];
         
-        // console.log(frutas[0]); // Se visualiza: manzana
-        // console.log(frutas[2]); // Se visualiza: plátano
-        // console.log(frutas.length); // Se visualiza: 4
+        console.log(frutas[0]); // Se visualiza: manzana
+        console.log(frutas[2]); // Se visualiza: plátano
+        console.log(frutas.length); // Se visualiza: 4
 
-        // let animales = ["perro", "gato", "elefante", "tigre", "león"];
+        let animales = ["perro", "gato", "elefante", "tigre", "león"];
 
-        // console.log(animales[1]); // Se visualiza: gato
-        // console.log(animales[3]); // Se visualiza: tigre
-        // console.log(animales.length); // Se visualiza: 5
+        console.log(animales[1]); // Se visualiza: gato
+        console.log(animales[3]); // Se visualiza: tigre
+        console.log(animales.length); // Se visualiza: 5
+    
+    //Concatenacion
+        const nombre = 'Livni';
+        const apellido = 'Martinez'
+        
+        console.log(nombre + ' ' + apellido);
