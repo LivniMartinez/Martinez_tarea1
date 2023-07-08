a0bad0a3a5f9bb4981d226d9c74ef3fde2ef1493
Livni <llmc0383@gmail.com>
2023-07-07 23:52:04 -0600

    se agregaron los diferentes metidos, arrays y ejemplos

--- script.js
+++ script.js
@@ -30,6 +30,7 @@
         console.log(datoNulo);
     
     //6. Objetos
+        //ej1
         let persona = {
             nombre: "Livni",
             edad: 29,
@@ -43,6 +44,7 @@
         console.log(persona.edad); 
         console.log(persona.mascota);
 
+        //ej2
         let barco = {
             marca: "Kukulkan",
             modelo: "Gc-1051",
@@ -55,13 +57,64 @@
         console.log(barco.marca);
         console.log(barco.modelo); 
         console.log(barco.capacidad); 
-        
+
+        //utilizando var
+
+        //ej3
+
+        var producto = {
+            nombre: "Teléfono",
+            precio: 299.99,
+            marca: "Samsung",
+            enStock: true
+          };
+          
+          console.log(producto.marca); // Sintaxis de punto
+          console.log(producto['marca']); // Sintaxis corchete
+  
+       //Desestructuracion
+       
+       var libro = {
+            titulo: "JavaScript for Beginners",
+            autor: "John Smith",
+            publicado: 2021
+        };
+      
+      const {autor} = libro;   
+
+
+
     //7. Arreglos
         let frutas = ["manzana", "naranja", "plátano", "uva"];
         
         console.log(frutas[0]); // Se visualiza: manzana
         console.log(frutas[2]); // Se visualiza: plátano
         console.log(frutas.length); // Se visualiza: 4
+
+        //metodos
+
+        //Push agrega uno o mas elementos
+        let frutas2 = ["manzana", "naranja"];
+        frutas2.push("plátano", "uva");
+        console.log(frutas); // Salida: ["manzana", "naranja", "plátano", "uva"]
+
+        //Pop elimina el ultimo elemento y lo devuelve
+        let frutas3 = ["manzana", "naranja", "plátano", "uva"];
+        let frutaEliminada = frutas3.pop();
+        console.log(frutas); // Salida: ["manzana", "naranja", "plátano"]
+        console.log(frutaEliminada); // Salida: uva
+
+        //Unshift agrega uno o mas elementos al inicio y devuelve la nueva longitud
+        let frutas4 = ["manzana", "naranja"];
+        frutas4.unshift("plátano", "uva");
+        console.log(frutas4); // Salida: ["plátano", "uva", "manzana", "naranja"]
+
+        //Shift elimina el primer elemento del arreglo
+        let frutas5 = ["plátano", "uva", "manzana", "naranja"];
+        let fruta5Eliminada = frutas5.shift();
+        console.log(frutas); // Salida: ["uva", "manzana", "naranja"]
+        console.log(frutaEliminada); // Salida: plátano
+
 
         let animales = ["perro", "gato", "elefante", "tigre", "león"];
 
@@ -75,7 +128,79 @@
         
         console.log(nombre + ' ' + apellido);
 
+        let nom = "Livni", antiguedad = 10883;
+        console.log(nom+antiguedad);
+
     //Array
 
     let array = [1, 2, 3, 'Livni'];
     console.log(array);
+
+
+    //Operadores matematicos
+    
+    //Algebraicos
+    let resultadoSuma = 5 + 3;
+    console.log(resultadoSuma); //Se visualiza 8
+
+    let resultadoMultiplicacion = 4 * 6;
+    console.log(resultadoMultiplicacion); //Se visualiza 24
+
+    //NaN NUMBER A NUMBER
+
+    //Divison por 0
+    let resultado = 10 / 0;
+    console.log(resultado); // Salida: NaN
+
+    //Operacion invalida
+
+    let numero = NaN;
+    let resultado2 = numero + 5;
+    console.log(resultado2); // Salida: NaN
+
+    //MATH
+
+    //Floor redondea hacia abajo al entero más cercano.
+
+    let num = 3.7;
+    let numeroRedondeado = Math.floor(num);
+    console.log(numeroRedondeado); // Salida: 3
+
+    //Max para obtener el valor máximo de un conjunto de números.
+
+    let numero1 = 3;
+    let numero2 = 7;
+    let numero3 = 5;
+
+    let maximo = Math.max(numero1, numero2, numero3);
+    console.log(maximo); // Salida: 7
+
+    //INCREMENTOS
+
+    //INCREMENTO DE UN NUMERO
+    let contador = 5;
+    contador++;
+    console.log(contador); // Salida: 6
+
+    //INCREMENTO DE UNA EXPRESION
+    let x = 3;
+    let y = 2;
+    let resul = x++ + y;
+    console.log(resul); // Salida: 5
+    console.log(x); // Salida: 4
+
+    //UTILIZANDO XOR
+
+    let a = true;
+    let b = false;
+
+    let resp = a ^ b;
+    console.log(resp); // Salida: true
+
+    //UTILIZANDO AND
+
+    let L = 5;
+    let M = 10;
+
+    let respuesta = x > 0 && y < 20;
+    console.log(respuesta); // Salida: true
